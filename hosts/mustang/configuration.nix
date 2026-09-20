920e51249c0420f1da668c01b9f88f17fb1b9244
Josh Cowen <josh.cowen@icloud.com>
2026-09-20 22:45:03 +0100

    updated configuration and hardware-configuration.nix locations and modules
    updating configuration files to be more common for better consistancy
    once multiple hosts are running nix system.

---
+++ hosts/mustang/configuration.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./modules/ly.nix
+    ../../modules/common.nix
+  ];
+
+  networking.hostName = "mustang"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      open = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    i2c = {
+      enable = true;
+      group = "i2c";
+    };
+  };
+
+  services = {
+    xserver.videoDrivers = [ "nvidia" ];
+  };
+
+  users.users.josh = {
+    extraGroups = [
+    "i2c"
+    ];
+  };
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
