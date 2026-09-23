82d326a3568dbcbbb2b2f29e21b6ad61da78a2bb
Josh Cowen <josh.cowen@icloud.com>
2026-09-23 21:24:16 +0100

    update ssh config to reflect future updates to default configuration

--- home/modules/programs/ssh.nix
+++ home/modules/programs/ssh.nix
@@ -3,6 +3,20 @@
 {
   programs.ssh = {
     enable = true;
+    settings."*" = {
+      forwardAgent = true;
+      addKeysToAgent = false;
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
+
+    enableDefaultConfig = false;
     extraConfig = ''
       IdentityAgent "~/.1password/agent.sock"
       setEnv TERM=xterm-256color
